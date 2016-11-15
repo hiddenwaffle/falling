0e2de545a98c5ee92b5d24aa60baf1a2463f6898
hiddenwaffle <hiddenwaffle@users.noreply.github.com>
2016-11-15 15:31:34 -0600

    Fix spelling error

--- src/scripts/view/standee/standee-animation.ts
+++ src/scripts/view/standee/standee-animation.ts
@@ -16,7 +16,7 @@ export class StandeeAnimation {
         let material = new THREE.SpriteMaterial({map: this.textureWrapper.texture});
 
         this.sprite = new THREE.Sprite(material);
-        this.sprite.scale.set(1, 1.5); // Just aspect ratio for 48 x 72 size frames. 
+        this.sprite.scale.set(1, 1.5); // Adjust aspect ratio for 48 x 72 size frames. 
         this.group.add(this.sprite);
     }
 
