efc667c31b7c9371dd07d20fef4397e1cb1fb0e4
hiddenwaffle <hiddenwaffle@users.noreply.github.com>
2016-11-23 15:27:07 -0600

    Tween falling sequence

--- src/scripts/model/board/falling-sequencer.ts
+++ src/scripts/model/board/falling-sequencer.ts
@@ -1,38 +1,63 @@
+declare const TWEEN: any;
+
+const FALL_TIME_MS = 1000;
+
 interface FallingBoard {
     calculateHighestColumn(): number;
     removeBottomLine(): void;
     resetAndPlay(): void
 }
 
-const STEP_DELAY = 50;
+class FallGuide {
+    lastHeight: number;
+    tweenedHeight: number;
+    elapsed: number;
+}
 
 export class FallingSequencer {
 
-    private finished: boolean;    
     private board: FallingBoard;
-    private currentStepTimeLeft: number;
+    private fallTween: any;
+    private fallTweenElapsed: number;
+    private fallGuide: FallGuide;
 
     constructor(board: FallingBoard) {
-        this.finished = true;
         this.board = board;
-        this.currentStepTimeLeft = STEP_DELAY;
+        this.fallTween = null;
+        this.fallGuide = new FallGuide();
     }
 
     resetAndPlay() {
-        this.finished = false;
-        this.currentStepTimeLeft = STEP_DELAY;
+        this.fallGuide.lastHeight = this.fallGuide.tweenedHeight = this.board.calculateHighestColumn();
+        this.fallGuide.elapsed = 0;
+
+        this.fallTween = new TWEEN.Tween(this.fallGuide)
+            .to({tweenedHeight: 0}, FALL_TIME_MS)
+            .easing(TWEEN.Easing.Cubic.In)
+            .onComplete(() => {
+                this.fallTween = null;
+                this.board.resetAndPlay();
+            })
+            .start(this.fallGuide.elapsed);
     }
 
+    /**
+     * Doing this in two parts because onComplete() can set the tween to null.
+     */
     step(elapsed: number) {
-        if (this.finished === false) {
-            this.currentStepTimeLeft -= elapsed;
-            if (this.currentStepTimeLeft <= 0) {
-                this.currentStepTimeLeft = STEP_DELAY;
-                this.board.removeBottomLine();
-                if (this.board.calculateHighestColumn() <= 0) {
-                    this.finished = true;
-                    this.board.resetAndPlay();
+        if (this.fallTween != null) {
+            this.fallGuide.elapsed += elapsed;
+            this.fallTween.update(this.fallGuide.elapsed);
+        }
+
+        if (this.fallTween != null) {
+            let newHeight = Math.ceil(this.fallGuide.tweenedHeight);
+            if  (this.fallGuide.lastHeight > newHeight) {
+                let diff = this.fallGuide.lastHeight - newHeight;
+                for (let idx = 0; idx < diff; idx++) {
+                    this.board.removeBottomLine();
                 }
+                this.fallGuide.lastHeight = newHeight;
             }
         }
     }
