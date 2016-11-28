2be6f04925292dacd52aa4c15eb01db4a5084cd3
hiddenwaffle <hiddenwaffle@users.noreply.github.com>
2016-11-28 14:38:19 -0600

    Calculate expected NPCs on board using gameState

--- src/scripts/model/npc/release-timer.ts
+++ src/scripts/model/npc/release-timer.ts
@@ -1,26 +1,53 @@
+import {GameStateType, gameState} from '../../game-state';
+
 // Starting position counts used in initialization.
 export const TOTAL_NPCS = 40;
 
 const TIME_UNTIL_EVERYONE_ON_SCREEN = 1.25 * 60 * 1000; // 75 seconds
 const NPCS_PER_SECOND = TIME_UNTIL_EVERYONE_ON_SCREEN / TOTAL_NPCS;
-const STARTING_COUNT = 5;
+const INTRO_STARTING_COUNT = 5;
 
 class ReleaseTimer {
 
+    private introTimeElapsed: number;
     private playTimeElapsed: number;
 
     constructor() {
+        this.introTimeElapsed = 0;
         this.playTimeElapsed = 0;
     }
 
     start() {
+        this.introTimeElapsed = 0;
         this.playTimeElapsed = 0;
     }
 
     step(elapsed: number): number {
+        let expectedInPlay = 0;
+
+        switch (gameState.getCurrent()) {
+            case GameStateType.Intro:
+                expectedInPlay = this.stepIntro(elapsed);
+                break;
+            case GameStateType.Playing:
+                expectedInPlay = this.stepPlaying(elapsed);
+                break;
+            default:
+                console.log('should not get here');
+        }
+
+        return expectedInPlay;
+    }
+
+    stepIntro(elapsed: number): number {
+        this.introTimeElapsed += elapsed;
+        return INTRO_STARTING_COUNT;
+    }
+
+    stepPlaying(elapsed: number): number {
         this.playTimeElapsed += elapsed;
 
-        let expectedInPlay = STARTING_COUNT + Math.floor(this.playTimeElapsed / NPCS_PER_SECOND);
+        let expectedInPlay = Math.floor(this.playTimeElapsed / NPCS_PER_SECOND);
         if (expectedInPlay > TOTAL_NPCS) {
             expectedInPlay = TOTAL_NPCS;
         }
